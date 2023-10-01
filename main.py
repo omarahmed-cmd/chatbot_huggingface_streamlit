5fb5f77266f0e8f6adb5422b19ec553a1be0c3a0
omarahmed-cmd <60565615+omarahmed-cmd@users.noreply.github.com>
2023-10-01 07:01:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,3 @@
-# import streamlit as sl
-# from transformers import pipeline
-
-# # Create a placeholder for the pipeline
-# text_gen_pipe = None
-
-# sl.title('Owais Chatbot') 
-# sl.markdown('---')
-
-# prompt = sl.text_input("Enter your question:")
-
-# pipe = pipeline("text2text-generation", model="declare-lab/flan-alpaca-gpt4-xl")
-# respond = pipe(prompt)
-# sl.write(respond[0]['generated_text'])
-
-# import streamlit as st
-# from transformers import pipeline
-
-# # Create a placeholder for the pipeline
-# text_gen_pipe = None
-
-# st.title('Owais Chatbot')
-# st.markdown('---')
-
-# prompt = st.text_input("Enter your question:")
-
-# if prompt:
-#     if text_gen_pipe is None:
-#         text_gen_pipe = pipeline("text2text-generation", model="declare-lab/flan-alpaca-gpt4-xl")
-
-#     respond = text_gen_pipe(prompt, max_length=230)  # Increase max_length to generate longer responses
-#     st.write(respond[0]['generated_text'])
-
 import streamlit as st
 import asyncio
 from transformers import pipeline
